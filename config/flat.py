7eca88cb0a7904a59eb6de5dafdcdbb750e6413f
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2018-01-18 13:44:34 -0600

    Comment on override of defaults

--- config/flat.py
+++ config/flat.py
@@ -1,2 +1,2 @@
-config.isr.doLinearize = False
-config.isr.doDefect = False
+config.isr.doLinearize = False  # we currently have no linearity model
+config.isr.doDefect = False  # we currently have no defect models
